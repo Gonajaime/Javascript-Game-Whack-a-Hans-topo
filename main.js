8cd8f5b63119bf181474e81bc3da407ec87c7a50
Gonajaime <jaime.gonzalez@bootcamp-upgrade.com>
2023-01-08 20:01:21 +0100

    second

--- main.js
+++ main.js
@@ -6,52 +6,46 @@ let score = 0;
 let timeUp = false;
 let lastHole;
 
-function ramdomTime(min, max){
-    return Math.round(Math.random() * (max - min) + min);
+function ramdomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
 }
-
 
 function ramdomHole(holes) {
-    const randomHans = Math.floor(Math.random() * holes.length);
-    const hole = holes[randomHans]
-    if (hole === lastHole) {
-        console.log("es el mismo hueco")
-        return ramdomHole(holes);
-        
-    } lastHole = hole;
-    return hole;
-
+  const randomHans = Math.floor(Math.random() * holes.length);
+  const hole = holes[randomHans];
+  if (hole === lastHole) {
+    console.log("es el mismo hueco");
+    return ramdomHole(holes);
+  }
+  lastHole = hole;
+  return hole;
 }
 
-
-function peep(){
-    const time = ramdomTime(200,1000);
-    const topo = ramdomHole(holes);
-    topo.classList.add("up");
-    setTimeout(() => {
-        topo.classList.remove("up");
-        if(!timeUp) peep();
-        }, time);
-     }
-
-
+function peep() {
+  const time = ramdomTime(200, 1000);
+  const topo = ramdomHole(holes);
+  topo.classList.add("up");
+  setTimeout(() => {
+    topo.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
+}
 
 function startGame() {
-    scoreBoard.textContent = 0;
-    timeUp = false;
-    score = 0;
-    peep();
-    setTimeout(() => timeUp = true, 15000)
- //muestra topos aleatoriamente durante 15 segundos
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  setTimeout(() => (timeUp = true), 15000);
+  //muestra topos aleatoriamente durante 15 segundos
 }
 
-function wack (e){
-    //para que no cuente un click simulado desde js y solo recoja el del usuario.
-    if(!e.isTrusted) return; 
-    this.classList.remove("up");
-    scoreBoard.textContent = score;
-    score++;
+function wack(e) {
+  //para que no cuente un click simulado desde js y solo recoja el del usuario.
+  if (!e.isTrusted) return;
+  this.classList.remove("up");
+  scoreBoard.textContent = score;
+  score++;
 }
 
-moles.forEach(topo => topo.addEventListener("click", wack));
-
+moles.forEach((topo) => topo.addEventListener("click", wack));
